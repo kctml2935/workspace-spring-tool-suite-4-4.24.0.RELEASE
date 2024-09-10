a816936fbbc3806510a744a65a8ce0958244b6e9
kctml2935 <kctml2935@gmail.com>
2024-09-10 11:12:58 +0900

    ...

--- servletSite/src/main/java/com/itwill/servlet/GugudanTextServlet.java
+++ servletSite/src/main/java/com/itwill/servlet/GugudanTextServlet.java
@@ -9,7 +9,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class GugudanTextServlet extends HttpServlet {
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request,
+						   HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/plain;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		for(int i=1; i<10;i++) {
